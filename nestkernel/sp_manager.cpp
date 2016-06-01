dde03ac052da353f37c7f1f505c585fdfae2b3a4
s.diaz <s.diaz@fz-juelich.de>
2016-06-01 11:04:19 +0200

    Added correction in get status so that the model of the synapse is also included for each registered pair of synaptic elements

--- nestkernel/sp_manager.cpp
+++ nestkernel/sp_manager.cpp
@@ -97,6 +97,9 @@ SPManager::get_status( DictionaryDatum& d )
       sp_synapse, names::pre_synaptic_element, ( *i )->get_pre_synaptic_element_name() );
     def< std::string >(
       sp_synapse, names::post_synaptic_element, ( *i )->get_post_synaptic_element_name() );
+    def< std::string >( sp_synapse,
+      names::model,
+       kernel().model_manager.get_synapse_prototype( ( *i )->get_synapse_model(), 0 ).get_name() );
     std::stringstream syn_name;
     syn_name << "syn" << ( sp_conn_builders_.end() - i );
     def< DictionaryDatum >( sp_synapses, syn_name.str(), sp_synapse );
